09eaab4f3bf881474cb4128b62b32b0170c98e92
Vinicius Araujo Lima <104456015+viniciusAL21@users.noreply.github.com>
2024-04-03 10:38:55 -0300

    Update urls.py
    adicionei as urls das novas views que fiz.

--- SaborReserva/api/urls.py
+++ SaborReserva/api/urls.py
@@ -5,9 +5,11 @@ from .views import MinhaAPIView
 
 urlpatterns = [
 	path('', views.api_geral, name='home_api'),
-    path('all_vendedores/', views.listar_vendedores, name='listar_vendedores'),
-    path('all_lanches/', views.listar_lanches, name='listar_lanches'),
-    path('create/vendedor', views.criar_vendedor, name='adicionaVendedor'),
-    path('create/lanche',views.criar_lanche,name='adicionaLanche'),
+    path('listar/vendedores', views.listar_vendedores, name='listar_vendedores'),
+    path('listar/lanches', views.listar_lanches, name='listar_lanches'),
+    path('listar/vendedor/<int:pk>/', views.listar_vendedor, name='listar_vendedor'),
+    path('listar/lanche/<int:pk>/', views.listar_lanche, name='listar_lanche'),
+    path('create/vendedor', views.criar_vendedor, name='adiciona_vendedor'),
+    path('create/lanche',views.criar_lanche,name='adiciona_lanche'),
     path('minha-api/', MinhaAPIView.as_view(), name='minha-api'),
 ]
